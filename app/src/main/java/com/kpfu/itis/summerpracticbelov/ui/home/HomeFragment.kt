3f25837d6cc267a6ee15247533f33174f98ed3be
Fenikonir <belovemil@gmail.com>
2023-06-29 17:05:28 +0300

    First HomeTask (29/06/2023)

--- app/src/main/java/com/kpfu/itis/summerpracticbelov/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kpfu/itis/summerpracticbelov/ui/home/HomeFragment.kt
@@ -1,15 +1,27 @@
 package com.kpfu.itis.summerpracticbelov.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.kpfu.itis.summerpracticbelov.databinding.FragmentHomeBinding
 
+
 class HomeFragment : Fragment() {
+
+    private lateinit var button: Button
+    private lateinit var editTextName: EditText
+    private lateinit var editTextHeight: EditText
+    private lateinit var editTextWeight: EditText
+    private lateinit var editTextAge: EditText
+    private lateinit var resultText: TextView
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -25,14 +37,67 @@ class HomeFragment : Fragment() {
         val homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
 
+
+
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        button = binding.button;
+        editTextName = binding.editTextName
+        editTextHeight = binding.editTextHeight
+        editTextWeight = binding.editTextWeight
+        editTextAge = binding.editTextAge
+        resultText = binding.resultText
+
+
         return root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener {
+            val name = editTextName.text.toString()
+            val height = editTextHeight.text.toString()
+            val weight = editTextWeight.text.toString()
+            val age = editTextAge.text.toString()
+
+            val maxHeight = 250
+            val minHeight = 0
+            val maxAge = 150
+            val minAge = 0
+
+            if (height.isNotEmpty() && height.toDouble() > maxHeight) {
+                editTextHeight.error = "Неверный Рост"
+                return@setOnClickListener
+            }
+
+            if (weight.isNotEmpty() && weight.toDouble() > maxHeight) {
+                editTextWeight.error = "Неверный  Вес"
+                return@setOnClickListener
+            }
+
+            if (age.isNotEmpty() && age.toInt() > maxAge) {
+                editTextAge.error = "Неверный Возраст"
+                return@setOnClickListener
+            }
+
+            val calories: Double = (weight.toDouble() * 10).toDouble() // Калории
+
+            val nds: Double = (weight.toDouble() * height.toDouble())// НДС
+
+            val mortgage: Double = (age.toDouble() * 1000).toDouble() // Example calculation for mortgage
+
+            val horoscope = "Гороскоп какой-то" // Example calculation for horoscope
+
+            val ndfl: Double = (weight.toDouble() / height.toDouble()).toDouble() // Ex
+
+            val message = "Результат\nКалории: $calories\nНДС: $nds\nMortgage: $mortgage\nГороскоп: $horoscope\nНДФЛ: $ndfl"
+            resultText.text = message
+            resultText.visibility = VISIBLE
+
+        }
     }
 
     override fun onDestroyView() {
